58fb04249a677a85028b982c9d64b376f3875ea6
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-12 23:31:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(array)
+  if array.size ==  0 
+    puts "The line is currently empty."
+  elsif array.size > 0 
+    new_array = []
+    array.each_with_index do |name, index|
+    new_array << "#{index + 1}. #{name}"
+    end
+    puts "The line is currently: " + new_array.join(" ")
+  end
+end
+
+def take_a_number(array, name)
+    array << name
+    array.each_with_index do |name, index|
+      puts "Welcome, #{name}. You are number #{index + 1} in line."
+    end
+end
+  
+
