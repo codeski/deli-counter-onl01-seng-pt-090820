bbb176a7d5c43b00bf18e666cf8c443885a8cb43
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-12 23:36:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 def take_a_number(array, name)
     array << name
     array.each_with_index do |name, index|
-      puts "Welcome, #{name.last}. You are number #{index.last + 1} in line."
+      puts "Welcome, #{name[-1]. You are number #{index[-1] + 1} in line."
     end
 end
   
