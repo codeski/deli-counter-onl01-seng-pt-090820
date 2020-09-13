22a356886bd1d1cbc584664e54345af7436d46ba
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-13 00:21:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(array)
   if array.size == 0 
     puts "There is nobody waiting to be served!"
   else
-    "Now serving #{name}."
+    "Now serving #{array.unshift}."
     array.shift
   end
 end
